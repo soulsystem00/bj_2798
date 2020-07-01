bdeef7b05d903554a43012108681f2cbf47f5a06
soulsystem00 <63180108+soulsystem00@users.noreply.github.com>
2020-07-01 21:44:04 +0900

    초기 커밋.

---
+++ bj_2798/bj_2798.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Cardcnt;
+    int MAXtotal;
+    cin >> Cardcnt >> MAXtotal;
+
+    int* card = new int[Cardcnt];
+
+    for (int i = 0; i < Cardcnt; i++)
+    {
+        cin >> card[i];
+    }
+
+    int MAXcombi = Cardcnt * (Cardcnt - 1) * (Cardcnt - 2) / 6;
+    int cnt = 0;
+    int* combi = new int[MAXcombi];
+    int MAXvalue = 0;
+    for (int i = 0; i < MAXcombi; i++)
+    {
+        combi[i] = 0;
+    }
+
+    for (int i = 0; i < Cardcnt; i++)
+    {
+        for (int j = i + 1; j < Cardcnt; j++)
+        {
+            for (int k = j + 1; k < Cardcnt; k++)
+            {
+                combi[cnt] = card[i] + card[j] + card[k];
+                
+                cnt++;
+                for (int l = 0; l < cnt-1; l++)
+                {
+                    if (combi[l] == combi[cnt-1])
+                    {
+                        cnt--;
+                        break;
+                    }
+                        
+                }
+            }
+        }
+    }
+
+    for (int i = MAXcombi-1; i >= 0; i--)
+    {
+        if (combi[i] <= MAXtotal && combi[i] != 0 && combi[i]>MAXvalue)
+        {
+            MAXvalue = combi[i];
+        }
+    }
+
+    cout << MAXvalue << endl;
+}
+
