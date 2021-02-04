8a26014a4ba1916fc88120c6d27fdb5db9cf503e
soulsystem00 <63180108+soulsystem00@users.noreply.github.com>
2021-02-04 20:01:02 +0900

    new

--- bj_2798/bj_1931.cpp
+++ bj_2798/bj_1931.cpp
@@ -8,7 +8,7 @@
 //	int y;
 //	int z;
 //}Point;
-//
+
 //void merge(Point list[], int first, int mid, int end)
 //{
 //	int size = end - first + 1;
@@ -21,7 +21,13 @@
 //
 //	while (i <= mid && j <= end)
 //	{
-//		if (list[i].z < list[j].z)
+//		if (list[i].y < list[j].y)
+//		{
+//			tmp[k] = list[i];
+//			k++;
+//			i++;
+//		}
+//		else if (list[i].y == list[j].y && list[i].x < list[j].x)
 //		{
 //			tmp[k] = list[i];
 //			k++;
@@ -89,7 +95,7 @@
 //	int end = 0;
 //	int cnt = 0;
 //	Point* arr = (Point*)malloc(sizeof(Point) * N);
-//	int min = INT32_MAX;
+//	int min = 2147483647;
 //	int max = 0;
 //	for (int i = 0; i < N; i++)
 //	{
@@ -102,37 +108,58 @@
 //	}
 //
 //	merge_sort(arr, 0, N - 1);
-//
-//	int* room = (int*)calloc(max + 1, sizeof(int));
-//	for (int i = 0; i < N; i++)
+//	start = arr[0].x;
+//	end = arr[0].y;
+//	cnt++;
+//	for (int i = 1; i < N; i++)
 //	{
-//		int j = arr[i].x;
-//		for (; j < arr[i].y; j++)
+//		if (arr[i].x >= end && arr[i].y >= end)
 //		{
-//			if (room[j] == 1)
-//				break;
-//		}
-//		if (j == arr[i].y)
-//		{
-//			for (j = arr[i].x; j < arr[i].y; j++)
-//			{
-//				room[j] = 1;
-//			}
 //			cnt++;
+//			start = arr[i].x;
+//			end = arr[i].y;
 //		}
 //	}
 //	cout << cnt;
-//	//print(arr, N);
-//
-//	//for (int i = 0; i < N; i++)
-//	//{
-//	//	if (start != arr[i].x && end < arr[i].x)
-//	//	{
-//	//		//printf("%d %d\n", arr[i].x, arr[i].y);
-//	//		start = arr[i].x;
-//	//		end = arr[i].y;
-//	//		cnt++;
-//	//	}
-//	//}
-//	//cout << cnt;
 //}
+
+
+//// 정렬 안하는 코드
+//// 정렬을 안하면 틀림
+////
+////#include <iostream>
+////
+////using namespace std;
+////typedef struct Point
+////{
+////	int x;
+////	int y;
+////}Point;
+////
+////int main()
+////{
+////	int N;
+////	cin >> N;
+////	int start = 0;
+////	int end = 0;
+////	int cnt = 0;
+////	Point* arr = (Point*)malloc(sizeof(Point) * N);
+////	for (int i = 0; i < N; i++)
+////	{
+////		cin >> arr[i].x >> arr[i].y;
+////	}
+////
+////	start = arr[0].x;
+////	end = arr[0].y;
+////	cnt++;
+////	for (int i = 1; i < N; i++)
+////	{
+////		if (arr[i].x >= end && arr[i].y >= end)
+////		{
+////			cnt++;
+////			start = arr[i].x;
+////			end = arr[i].y;
+////		}
+////	}
+////	cout << cnt;
+////}
