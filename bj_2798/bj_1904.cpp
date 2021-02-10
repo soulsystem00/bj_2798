3086c491607c967fec901a81c6ea185fb4d3d045
soulsystem00 <63180108+soulsystem00@users.noreply.github.com>
2021-02-10 20:22:15 +0900

    new

--- bj_2798/bj_1904.cpp
+++ bj_2798/bj_1904.cpp
@@ -1,19 +1,20 @@
-//#include<iostream>
-//
-//using namespace std;
-//
-//int arr[1000001];
-//int main()
-//{
-//	arr[0] = 1;
-//	arr[1] = 2;
-//	for (int i = 2; i < 1000001; i++)
-//	{
-//		arr[i] = (arr[i - 2] + arr[i - 1]) % 15746;
-//	}
-//
-//	int num;
-//	cin >> num;
-//
-//	cout << arr[num-1];
-//}
+#include<iostream>
+
+using namespace std;
+
+int arr[1000001];
+int main()
+{
+	int num;
+	cin >> num;
+	arr[0] = 1;
+	arr[1] = 2;
+	for (int i = 2; i < num; i++)
+	{
+		arr[i] = (arr[i - 2] + arr[i - 1]) % 15746;
+	}
+
+	
+
+	cout << arr[num-1];
+}
