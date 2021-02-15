54f30963fe396f56a17092a28592196522caef82
soulsystem00 <63180108+soulsystem00@users.noreply.github.com>
2021-02-15 20:17:03 +0900

    new

--- bj_2798/bj_1149.cpp
+++ bj_2798/bj_1149.cpp
@@ -1,51 +1,26 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//typedef struct House
-//{
-//	int red;
-//	int blue;
-//	int green;
-//}house;
-//
-//int function()
-//{
-//	return 0;
-//
-//
-//}
-//
-//
-//int main()
-//{
-//	int num;
-//	cin >> num;
-//	//house* arr = new house[num];
-//	int** arr = new int* [num];
-//	for (int i = 0; i < num; i++)
-//	{
-//		arr[i] = new int[3];
-//	}
-//	for (int i = 0; i < num; i++)
-//	{
-//		cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
-//	}
-//
-//	for (int i = 0; i < num; i++)
-//	{
-//		printf("%d %d %d\n", arr[i][0], arr[i][1], arr[i][2]);
-//		//cout << arr[i][0] << arr[i][1] << arr[i][2];
-//	}
-//
-//	for (int i = 0; i < num; i++)
-//	{
-//		for (int j = 0; j < 3; j++)
-//		{
-//			cout << arr[i][j] << " ";
-//		}
-//		cout << endl;
-//	}
-//
-//	
-//}
+#include <iostream>
+
+using namespace std;
+
+typedef struct House
+{
+	int red;
+	int blue;
+	int green;
+}house;
+
+int function()
+{
+	return 0;
+
+
+}
+
+
+int main()
+{
+	int num;
+	cin >> num;
+
+	
+}
