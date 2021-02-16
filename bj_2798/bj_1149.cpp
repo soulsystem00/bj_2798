5056059d5d0476291e5f752b18cda45b6d897d81
soulsystem00 <63180108+soulsystem00@users.noreply.github.com>
2021-02-16 13:05:31 +0900

    new

--- bj_2798/bj_1149.cpp
+++ bj_2798/bj_1149.cpp
@@ -1,26 +1,26 @@
-#include <iostream>
-
-using namespace std;
-
-typedef struct House
-{
-	int red;
-	int blue;
-	int green;
-}house;
-
-int function()
-{
-	return 0;
-
-
-}
-
-
-int main()
-{
-	int num;
-	cin >> num;
-
-	
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//typedef struct House
+//{
+//	int red;
+//	int blue;
+//	int green;
+//}house;
+//
+//int function()
+//{
+//	return 0;
+//
+//
+//}
+//
+//
+//int main()
+//{
+//	int num;
+//	cin >> num;
+//
+//	
+//}
---
+++ bj_2798/bj_18258.cpp
@@ -0,0 +1,87 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+	int cnt;
+	cin >> cnt;
+	cin.ignore();
+	queue<int> q;
+	for (int i = 0; i < cnt; i++)
+	{
+		string str;
+		
+		getline(cin, str);
+		if (str == "pop")
+		{
+			if (q.empty())
+			{
+				//printf("%d\n", -1);
+				cout << "-1\n";
+			}
+			else
+			{
+				//printf("%d\n", q.front());
+				cout << q.front() << "\n";
+				q.pop();
+			}
+			
+		}
+		else if (str == "size")
+		{
+			//printf("%d\n", q.size());
+			cout << q.size() << "\n";
+		}
+		else if (str == "empty")
+		{
+			if (q.empty())
+			{
+				//printf("%d\n", 1);
+				cout << 1 << "\n";
+			}
+			else
+			{
+				//printf("%d\n", 0);
+				cout << 0 << "\n";
+			}
+		}
+		else if (str == "front")
+		{
+			if (q.empty())
+			{
+				//printf("%d\n", -1);
+				cout << -1 << "\n";
+			}
+			else
+			{
+				//printf("%d\n", q.front());
+				cout << q.front() << "\n";
+			}
+		}
+		else if (str == "back")
+		{
+			if (q.empty())
+			{
+				//printf("%d\n", -1);
+				cout << -1 << "\n";
+			}
+			else
+			{
+				//printf("%d\n", q.back());
+				cout << q.back() << "\n";
+			}
+		}
+		else
+		{
+			int num = stoi(str.substr(5, str.length()));
+			q.push(num);
+		}
+		
+	}
+}
