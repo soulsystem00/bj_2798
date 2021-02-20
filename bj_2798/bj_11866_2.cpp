600f748955f1c0bd300a20519c44b11e21cfcdf5
soulsystem00 <63180108+soulsystem00@users.noreply.github.com>
2021-02-20 20:27:22 +0900

    new

--- bj_2798/bj_11866_2.cpp
+++ bj_2798/bj_11866_2.cpp
@@ -1,47 +1,47 @@
-#include <iostream>
-#include <queue>
-
-using namespace std;
-
-int main()
-{
-	int cnt;
-	int num;
-	cin >> cnt >> num;
-
-	queue<int> q;
-	for (int i = 0; i < cnt; i++)
-	{
-		q.push(i + 1);
-	}
-
-
-	int curcnt = 1;
-	cout << "<";
-	while (!q.empty())
-	{
-
-		if (curcnt != num)
-		{
-			int tmp = q.front();
-			q.pop();
-			q.push(tmp);
-			curcnt++;
-		}
-		else
-		{
-			if (q.size() == 1)
-			{
-				cout << q.front();
-			}
-			else
-			{
-				cout << q.front() << ", ";
-			}
-			
-			q.pop();
-			curcnt = 1;
-		}
-	}
-	cout << ">";
-}
+//#include <iostream>
+//#include <queue>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int cnt;
+//	int num;
+//	cin >> cnt >> num;
+//
+//	queue<int> q;
+//	for (int i = 0; i < cnt; i++)
+//	{
+//		q.push(i + 1);
+//	}
+//
+//
+//	int curcnt = 1;
+//	cout << "<";
+//	while (!q.empty())
+//	{
+//
+//		if (curcnt != num)
+//		{
+//			int tmp = q.front();
+//			q.pop();
+//			q.push(tmp);
+//			curcnt++;
+//		}
+//		else
+//		{
+//			if (q.size() == 1)
+//			{
+//				cout << q.front();
+//			}
+//			else
+//			{
+//				cout << q.front() << ", ";
+//			}
+//			
+//			q.pop();
+//			curcnt = 1;
+//		}
+//	}
+//	cout << ">";
+//}
---
+++ bj_2798/bj_1966.cpp
@@ -0,0 +1,116 @@
+//#include <iostream>
+//#include <queue>
+//#include <algorithm>
+//using namespace std;
+//
+//bool function(int a, int b)
+//{
+//	return b < a;
+//}
+//
+//struct number
+//{
+//	int num;
+//	int index;
+//};
+//int main()
+//{
+//	int num;
+//	cin >> num;
+//	for (int i = 0; i < num; i++)
+//	{
+//		int asize, select;
+//		cin >> asize >> select;
+//		queue<int> q;
+//		int selectnum;
+//		for (int i = 0; i < asize; i++)
+//		{
+//			int tmp;
+//			cin >> tmp;
+//			q.push(tmp);
+//			if (i == select)
+//			{
+//				selectnum = tmp;
+//			}
+//		}
+//		int cnt = 1;
+//		int cntequal = 0;
+//		int index = 0;
+//		bool check = true;
+//		while (!q.empty())
+//		{
+//			int tmp = q.front();
+//			q.pop();
+//			if (tmp > selectnum)
+//			{
+//				cnt++;
+//			}
+//			else if (tmp == selectnum)
+//			{
+//
+//			}
+//			index++;
+//		}
+//		cout << cnt << endl;
+//	}
+//}
+
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+bool function(int a, int b)
+{
+	return b < a;
+}
+
+struct number
+{
+	int num;
+	int index;
+};
+int main()
+{
+	int num;
+	cin >> num;
+	for (int i = 0; i < num; i++)
+	{
+		int asize, select;
+		cin >> asize >> select;
+		queue<number> q;
+		int selectnum;
+		for (int i = 0; i < asize; i++)
+		{
+			number tmp2;
+			int tmp;
+			cin >> tmp;
+			tmp2.num = tmp;
+			tmp2.index = i;
+			q.push(tmp2);
+			if (i == select)
+			{
+				selectnum = tmp;
+			}
+		}
+		int cnt = 0;
+		int cntequal = 0;
+		int index = 0;
+		bool check = true;
+		while (!q.empty())
+		{
+			
+			number tmp = q.front();
+			q.pop();
+			if (tmp.num > selectnum)
+			{
+				cnt++;
+			}
+			else
+			{
+				q.push(tmp);
+			}
+		}
+		cout << cnt << endl;
+	}
+}
